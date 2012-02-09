510ab87d6f706aac6b8f2a3be23413e21ff5e0c5
Mime Čuvalo <mimecuvalo@gmail.com>
2012-02-09 16:16:22 +0100

    version 0.89.1 - fix issue that had firessh have a dependency on fireftp

--- src/content/js/etc/globals.js
+++ src/content/js/etc/globals.js
@@ -1,5 +1,5 @@
 var gConnection;                // the main connection - where the magic happens
-var gVersion  = "0.89";  // version of FireSSH we're using
+var gVersion  = "0.89.1";  // version of FireSSH we're using
 var gPlatform;                  // holds what platform we're on
 var gSiteManager;               // hold site manager data
 var gAccount;                   // name of the account we're connecting to
