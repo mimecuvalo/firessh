b31e19b7b058f7507e5c2c3251c16f50ad322d85
Mime Čuvalo <mimecuvalo@gmail.com>
2013-04-22 21:37:27 +0200

    tweak 256 colors and logging

--- src/content/js/connection/ssh2.js
+++ src/content/js/connection/ssh2.js
@@ -121,7 +121,7 @@ ssh2Mozilla.prototype = {
       this.client.load_host_keys(host_keys);
 
       var auth_success = function() {
-        self.client.invoke_shell('xterm', self.width, self.height, shell_success);
+        self.client.invoke_shell('xterm-256color', self.width, self.height, shell_success);
       };
 
       var write = function(out) {
